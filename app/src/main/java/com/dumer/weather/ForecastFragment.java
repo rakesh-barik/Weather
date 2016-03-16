96a6f972c650fd5a920b4294ac8a29aa7a061283
rakeshbarik <rakesh.barik@tavant.com>
2016-03-16 10:36:04 +0530

    forecast fragment

---
+++ app/src/main/java/com/dumer/weather/ForecastFragment.java
@@ -0,0 +1,90 @@
+package com.dumer.weather;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ForecastFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+    private ArrayAdapter<String> mForecastAdapter;
+
+    public ForecastFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
+
+        List<String> weatherArrayList = new ArrayList<>();
+        weatherArrayList.add("Today-Sunny-88/63");
+        weatherArrayList.add("Tomorrow-Foggy-70/56");
+        weatherArrayList.add("Wed-Cloudy-70/56");
+        weatherArrayList.add("Thu-Rainy-63/56");
+        weatherArrayList.add("Fri-Foggy-88/56");
+        weatherArrayList.add("Sat-Cloudy-63/56");
+        weatherArrayList.add("Sun-Rainy-70/56");
+
+        mForecastAdapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weatherArrayList);
+
+        ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
+    }
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
